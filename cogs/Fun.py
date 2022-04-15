2a59d541b3017d2bfef2fc2427e769d43a8e35cc
ShadowMagic896 <ryanoc1026@gmail.com>
2022-04-15 11:36:25 -0400

    stuff eeee

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -1,3 +1,4 @@
+from stat import filemode
 from turtle import pu
 import discord
 from discord.ext import commands
@@ -12,6 +13,7 @@ import pyautogui
 import pygetwindow
 import sys, os
 import asyncio
+import subprocess
 
 import time
 
@@ -23,7 +25,7 @@ class Fun(commands.Cog):
     
     @commands.hybrid_command(name="font")
     async def font(self, ctx, font: Literal[
-            "3-d", "ascii___", "banner3-D", "charset_", "gothic", "hollywood", "linux", "lockergnome", "lexible_", 
+            "0.1-d", "ascii___", "banner0.1-D", "charset_", "gothic", "hollywood", "linux", "lockergnome", "lexible_", 
             "platoon_", "mike", "mini", "mirror", "mnemonic",  "modern__", "morse", "moscow", "slant", "trek", 
             "wavy", "weird", "tomahawk", "usaflag", 
         ], *, text: str):
@@ -36,7 +38,7 @@ class Fun(commands.Cog):
             embed = fmte(ctx, t = "Rendering Finished!")
             if len(t) > 1990:
                 embed.set_footer(text = "Requested by {}\n[Tuncated because of size]".format(ctx.author))
-            await ctx.send("```{}```".format(t[:1990]), embed = embed)
+            await ctx.message.reply("```{}```".format(t[:1990]), embed = embed)
                 
         except pyfiglet.FontNotFound:
             embed = fmte(
@@ -44,7 +46,7 @@ class Fun(commands.Cog):
                 t = "Sorry, I can't find that font.",
                 d = "Maybe try another one, or use `>>help font` for all fonts",
             )
-            await ctx.send(embed = embed)
+            await ctx.message.reply(embed = embed)
     
     @commands.hybrid_group()
     @commands.is_owner()
@@ -59,14 +61,15 @@ class Fun(commands.Cog):
     async def ss(self, ctx: commands.Context, regionX:int = 0, regionY:int = 0, regionWidth:int = pyautogui.size()[0], regionHeight:int = pyautogui.size()[1]):
         pyautogui.screenshot(region=(regionX, regionY, regionWidth, regionHeight)).save("pyauto_resources/game_image.jpg")
         f = discord.File("pyauto_resources/game_image.jpg", filename = "Game_Image.jpg")
-        await ctx.send(file = f)
+        await ctx.message.reply(file = f)
 
     @gui.command()              
-    async def open(self, ctx, file_location: str = "C:\\Users\\ryano\\Downloads\\VisualBoyAdvance.exe"):
+    async def open(self, ctx: discord.ext.commands.Context, file_location: str = "C:\\Users\\ryano\\Downloads\\VisualBoyAdvance.exe"):
         os.startfile(file_location)
     
     @gui.command()
-    async def run_vsb(self, ctx):
+    async def run_vba(self, ctx, framerate: float = 1.0):
+        ms = await ctx.send("Loading emulator...")
         os.startfile("C:\\Users\\ryano\\Downloads\\VisualBoyAdvance.exe")
         
         while "VisualBoyAdvance" not in pyautogui.getAllTitles():
@@ -104,14 +107,35 @@ class Fun(commands.Cog):
         )
         pyautogui.screenshot().save("pyauto_resources/game_image.jpg")
         file = discord.File("pyauto_resources/game_image.jpg", filename = "Game_Image.jpg")
-        await ctx.send(embed=embed, file=file, view = VBA_View(ctx, ctx.bot))
+        ms = await ctx.message.reply(embed=embed, file=file, view = VBA_View(ctx, ctx.bot, ms))
+
+        while True:
+            embed = fmte(
+                ctx=ctx,
+                t="Playing: Pokemon: Fire Red",
+                d="For: {} minutes".format((time.time() - ctx.start_time) / 60)
+            )
+            pyautogui.screenshot().save("pyauto_resources/game_image.jpg")
+            file = discord.File("pyauto_resources/game_image.jpg", filename = "Game_Image.jpg")
+            view = VBA_View(ctx, ctx.bot,ms)
+            try:
+                await ms.edit(
+                    embed = embed,
+                    attachments = [file,],
+                    view = view
+                )
+            except discord.errors.NotFound:
+                await ctx.send("Emulator closed by user.")
+                break
+            await asyncio.sleep(float(framerate))
 
 
 class VBA_View(discord.ui.View):
-    def __init__(self, ctx, bot):
+    def __init__(self, ctx, bot, msg):
         super().__init__()
         self.ctx = ctx
         self.bot = bot
+        self.res_msg = msg
     
     @discord.ui.button(
         label = "L",
@@ -121,18 +145,10 @@ class VBA_View(discord.ui.View):
     async def leftTrigger(self, interaction: discord.Interaction, button: discord.ui.Button):
         if self.ctx.author != interaction.user:
             return
-        await self.ctx.defer()
-        pyautogui.keyDown("A")
-        await asyncio.sleep(0.2)
-        pyautogui.keyUp("A")
-        embed = fmte(
-            ctx=self.ctx,
-            t="Playing: Pokemon: Fire Red",
-            d="For: {} minutes".format((time.time() - self.ctx.start_time) / 60)
-        )
-        pyautogui.screenshot().save("pyauto_resources/game_image.jpg")
-        file = discord.File("pyauto_resources/game_image.jpg", filename = "Game_Image.jpg")
-        await interaction.message.edit(embed=embed,attachments=[file,],view=VBA_View(self.ctx,self.bot))
+        
+        pyautogui.keyDown("l")
+        await asyncio.sleep(1)
+        pyautogui.keyUp("l")
 
     @discord.ui.button(
         label = "🔼",
@@ -142,18 +158,10 @@ class VBA_View(discord.ui.View):
     async def upArrow(self, interaction: discord.Interaction, button: discord.ui.Button):
         if self.ctx.author != interaction.user:
             return
-        await self.ctx.defer()
+        
         pyautogui.keyDown("up")
-        await asyncio.sleep(0.2)
+        await asyncio.sleep(1)
         pyautogui.keyUp("up")
-        embed = fmte(
-            ctx=self.ctx,
-            t="Playing: Pokemon: Fire Red",
-            d="For: {} minutes".format((time.time() - self.ctx.start_time) / 60)
-        )
-        pyautogui.screenshot().save("pyauto_resources/game_image.jpg")
-        file = discord.File("pyauto_resources/game_image.jpg", filename = "Game_Image.jpg")
-        await interaction.message.edit(embed=embed,attachments=[file,],view=VBA_View(self.ctx,self.bot))
 
     @discord.ui.button(
         label = "R",
@@ -163,18 +171,10 @@ class VBA_View(discord.ui.View):
     async def rightTrigger(self, interaction: discord.Interaction, button: discord.ui.Button):
         if self.ctx.author != interaction.user:
             return
-        await self.ctx.defer()
-        pyautogui.keyDown("S")
-        await asyncio.sleep(0.2)
-        pyautogui.keyUp("S")
-        embed = fmte(
-            ctx=self.ctx,
-            t="Playing: Pokemon: Fire Red",
-            d="For: {} minutes".format((time.time() - self.ctx.start_time) / 60)
-        )
-        pyautogui.screenshot().save("pyauto_resources/game_image.jpg")
-        file = discord.File("pyauto_resources/game_image.jpg", filename = "Game_Image.jpg")
-        await interaction.message.edit(embed=embed,attachments=[file,],view=VBA_View(self.ctx,self.bot))
+        
+        pyautogui.keyDown("r")
+        await asyncio.sleep(1)
+        pyautogui.keyUp("r")
     
     @discord.ui.button(
         label = "◀️",
@@ -184,37 +184,19 @@ class VBA_View(discord.ui.View):
     async def leftArrow(self, interaction: discord.Interaction, button: discord.ui.Button):
         if self.ctx.author != interaction.user:
             return
-        await self.ctx.defer()
+        
         pyautogui.keyDown("left")
-        await asyncio.sleep(0.2)
+        await asyncio.sleep(1)
         pyautogui.keyUp("left")
-        embed = fmte(
-            ctx=self.ctx,
-            t="Playing: Pokemon: Fire Red",
-            d="For: {} minutes".format((time.time() - self.ctx.start_time) / 60)
-        )
-        pyautogui.screenshot().save("pyauto_resources/game_image.jpg")
-        file = discord.File("pyauto_resources/game_image.jpg", filename = "Game_Image.jpg")
-        await interaction.message.edit(embed=embed,attachments=[file,],view=VBA_View(self.ctx,self.bot))
     
     @discord.ui.button(
-        label = "  ",
-        disabled=True,
+        label = " ",
         style = discord.ButtonStyle.grey,
         row = 1
     )
-    async def nothing(self, interaction: discord.Interaction, button: discord.ui.Button):
+    async def blank(self, interaction: discord.Interaction, button: discord.ui.Button):
         if self.ctx.author != interaction.user:
             return
-        await self.ctx.defer()
-        embed = fmte(
-            ctx=self.ctx,
-            t="Playing: Pokemon: Fire Red",
-            d="For: {} minutes".format((time.time() - self.ctx.start_time) / 60)
-        )
-        pyautogui.screenshot().save("pyauto_resources/game_image.jpg")
-        file = discord.File("pyauto_resources/game_image.jpg", filename = "Game_Image.jpg")
-        await interaction.message.edit(embed=embed,attachments=[file,],view=VBA_View(self.ctx,self.bot))
     
     @discord.ui.button(
         label = "▶️",
@@ -224,18 +206,10 @@ class VBA_View(discord.ui.View):
     async def rightArrow(self, interaction: discord.Interaction, button: discord.ui.Button):
         if self.ctx.author != interaction.user:
             return
-        await self.ctx.defer()
+        
         pyautogui.keyDown("right")
-        await asyncio.sleep(0.2)
+        await asyncio.sleep(1)
         pyautogui.keyUp("right")
-        embed = fmte(
-            ctx=self.ctx,
-            t="Playing: Pokemon: Fire Red",
-            d="For: {} minutes".format((time.time() - self.ctx.start_time) / 60)
-        )
-        pyautogui.screenshot().save("pyauto_resources/game_image.jpg")
-        file = discord.File("pyauto_resources/game_image.jpg", filename = "Game_Image.jpg")
-        await interaction.message.edit(embed=embed,attachments=[file,],view=VBA_View(self.ctx,self.bot))
     
     @discord.ui.button(
         label = "A",
@@ -245,18 +219,10 @@ class VBA_View(discord.ui.View):
     async def AButton(self, interaction: discord.Interaction, button: discord.ui.Button):
         if self.ctx.author != interaction.user:
             return
-        await self.ctx.defer()
-        pyautogui.keyDown("z")
-        await asyncio.sleep(0.2)
-        pyautogui.keyUp("z")
-        embed = fmte(
-            ctx=self.ctx,
-            t="Playing: Pokemon: Fire Red",
-            d="For: {} minutes".format((time.time() - self.ctx.start_time) / 60)
-        )
-        pyautogui.screenshot().save("pyauto_resources/game_image.jpg")
-        file = discord.File("pyauto_resources/game_image.jpg", filename = "Game_Image.jpg")
-        await interaction.message.edit(embed=embed,attachments=[file,],view=VBA_View(self.ctx,self.bot))
+        
+        pyautogui.keyDown("a")
+        await asyncio.sleep(1)
+        pyautogui.keyUp("a")
     
     @discord.ui.button(
         label = "🔽",
@@ -266,18 +232,10 @@ class VBA_View(discord.ui.View):
     async def downArrow(self, interaction: discord.Interaction, button: discord.ui.Button):
         if self.ctx.author != interaction.user:
             return
-        await self.ctx.defer()
+        
         pyautogui.keyDown("down")
-        await asyncio.sleep(0.2)
+        await asyncio.sleep(1)
         pyautogui.keyUp("down")
-        embed = fmte(
-            ctx=self.ctx,
-            t="Playing: Pokemon: Fire Red",
-            d="For: {} minutes".format((time.time() - self.ctx.start_time) / 60)
-        )
-        pyautogui.screenshot().save("pyauto_resources/game_image.jpg")
-        file = discord.File("pyauto_resources/game_image.jpg", filename = "Game_Image.jpg")
-        await interaction.message.edit(embed=embed,attachments=[file,],view=VBA_View(self.ctx,self.bot))
     
     @discord.ui.button(
         label = "B",
@@ -287,18 +245,49 @@ class VBA_View(discord.ui.View):
     async def BButton(self, interaction: discord.Interaction, button: discord.ui.Button):
         if self.ctx.author != interaction.user:
             return
-        await self.ctx.defer()
-        pyautogui.keyDown("x")
-        await asyncio.sleep(0.2)
-        pyautogui.keyUp("x")
-        embed = fmte(
-            ctx=self.ctx,
-            t="Playing: Pokemon: Fire Red",
-            d="For: {} minutes".format((time.time() - self.ctx.start_time) / 60)
-        )
-        pyautogui.screenshot().save("pyauto_resources/game_image.jpg")
-        file = discord.File("pyauto_resources/game_image.jpg", filename = "Game_Image.jpg")
-        await interaction.message.edit(embed=embed,attachments=[file,],view=VBA_View(self.ctx,self.bot))
+        
+        pyautogui.keyDown("b")
+        await asyncio.sleep(1)
+        pyautogui.keyUp("b")
+    
+    @discord.ui.button(
+        label = "SEL",
+        style = discord.ButtonStyle.blurple,
+        row = 3
+    )
+    async def select(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if self.ctx.author != interaction.user:
+            return
+        
+        pyautogui.keyDown("enter")
+        await asyncio.sleep(1)
+        pyautogui.keyUp("enter")
+    
+    @discord.ui.button(
+        label = "❌",
+        style = discord.ButtonStyle.red,
+        row = 3
+    )
+    async def stop(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if self.ctx.author != interaction.user:
+            return
+        await self.res_msg.delete()
+        subprocess.Popen("taskkill /IM VisualBoyAdvance.exe")
+    
+    @discord.ui.button(
+        label = "SRT",
+        style = discord.ButtonStyle.blurple,
+        row = 3
+    )
+    async def start(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if self.ctx.author != interaction.user:
+            return
+        
+        pyautogui.keyDown("shiftright")
+        await asyncio.sleep(1)
+        pyautogui.keyUp("shiftright")
+    
+    
     
 
             
