9027026898362369ee16504fb00f5c883ec40bc8
Ryan Peckham <ryanoc1026@gmail.com>
2022-06-21 14:39:30 -0400

    Add environ example

---
+++ environ_example.py
@@ -0,0 +1,15 @@
+APPLICATION_ID: str = "" # https://discord.com/developers/applications
+BOT_KEY: str = "" # https://discord.com/developers/applications/APPLICATION_ID/bot
+QUIZAPI_KEY: str = "" # https://quizapi.io/docs/1.0/authentication
+DICT_API_KEY: str = "" # https://dictionaryapi.com/products/api-collegiate-dictionary
+X_RAPID_API_KEY: str = "" # https://rapidapi.com/
+OPENAI_KEY: str = "" # https://openai.com/api/
+LIBRARY_KEY: str = "" # https://libraries.io/api
+
+NSFW_PATH: str = "" # ...
+
+# POSTGRES
+# https://www.postgresql.org/download/
+# Username is most likely "postgres"
+DB_USERNAME: str = "" 
+DB_PASSWORD: str = ""
