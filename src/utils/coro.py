3d08a3009f9115e48fa81ba49c3e3a081f242942
Ryan Peckham <86629697+ShadowMagic896@users.noreply.github.com>
2022-06-24 21:48:44 -0400

    Update coro.py

--- src/utils/coro.py
+++ src/utils/coro.py
@@ -1,8 +1,14 @@
 import asyncio
 import functools
+import logging
 from typing import Any, Callable
 
 
 async def run(func: Callable, *args, **kwargs) -> Any:
+    if not callable(func):
+        if args and callable(args[0]):
+            logging.error("Remove loop argument from coro.run")
+            args = list(args)
+            func = args.pop(0)
     part = functools.partial(func, *args, **kwargs)
     return await asyncio.get_event_loop().run_in_executor(None, part)
